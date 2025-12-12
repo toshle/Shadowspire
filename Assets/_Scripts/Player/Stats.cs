bb8dcfc1ae2932df8ef60165d8ea7e6055007735
Rosen Rachev <rosen_ra4ev@abv.bg>
2025-12-12 20:24:51 +0200

    Update Stats.cs

--- Assets/_Scripts/Player/Stats.cs
+++ Assets/_Scripts/Player/Stats.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
-    public float MovementSpeed = 0f;
+    public float MovementSpeed = 0f; //Done
     public float Armor = 0f;
-    public float BonusDamage = 0f;
+    public float BonusDamage = 0f; //Done
     public float AttackSpread = 0f;
     public float AttackRange = 0f;
-    public int AttackPassThrough = 0;
-    public float AttackSpeed = 0f;
+    public int AttackPassThrough = 0; //Done
+    public float AttackSpeed = 0f; //Done
     public float LifeSteal = 0f;
-    public int HealthRegeneration = 0;
-    public bool HealOnLevelUp = false;
+    public float HealthRegeneration = 0f;
+    public bool HealOnLevelUp = false; //Done
     public bool ExplodingProjectiles = false;
 
 
